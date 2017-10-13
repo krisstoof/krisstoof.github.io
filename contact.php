4e1a814b61dd1b586a2e04639bd7d0a807b226d7
krisstoof <krisstoof1@gmail.com>
2017-10-13 09:37:35 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = 'krz.rzeszot@gmail.com';
+
+$header = "From: $email \r\n"; 
+$header.= "MIME-Version: 1.0\r\n"; 
+$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n"; 
+$header.= "X-Priority: 1\r\n"; 
+
+$status = mail($to, $name, $message, $header);
+
+if($status)
+{ 
+    echo '<p>Your mail has been sent!</p>';
+} else { 
+    echo '<p>Something went wrong, Please try again!</p>'; 
+}
